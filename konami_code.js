f1e25c80e805d24965f2bae62ee8b2c7d9f93b5b
satub <satu.barnhill@gmail.com>
2016-10-03 16:22:06 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,24 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // your code here
-}
+  var index = 0;
+  document.body.addEventListener('keydown', function(e) {
+    e.stopPropagation();
+    const key = parseInt(e.detail || e.which)
+
+
+      if (key === code[index]) {
+        console.log(`${index}`)
+        index++
+
+        if (index === code.length) {
+          alert("YOU DID IT!")
+          index = 0
+        }
+      } else {
+        console.log(`missed!!!`)
+        index = 0
+      }
+    })
+  }
+  init();
